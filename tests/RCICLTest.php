531a5068efaeea3c561149d8bbcb64cd4401ef87
Franco Sanllehi <francosanllehi@gmail.com>
2019-08-25 13:04:20 -0400

    Test Commit

---
+++ tests/RCICLTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DmACKGL\RCICL\Tests;
+
+use DmACKGL\RCICL\RCICL;
+use PHPUnit\Framework\TestCase;
+
+class RCICLTest extends TestCase
+{
+    /**
+     * @todo Crear Test
+     * @test
+     */
+    public function test_RCICL()
+    {
+
+    }
+}
